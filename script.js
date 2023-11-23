1dcd0abb56119dccd88f32dd294f4eedc796cbbd
haris936hk <hariskhan936.hk@gmail.com>
2023-11-23 22:33:50 +0500

    javascript ka code likh liya ha

---
+++ script.js
@@ -0,0 +1,10 @@
+// Your JavaScript code here
+function startMemoryTraining() {
+    // Logic for starting memory training game
+    console.log("Starting Memory Training Game");
+}
+
+function startAttentionTraining() {
+    // Logic for starting attention training game
+    console.log("Starting Attention Training Game");
+}
