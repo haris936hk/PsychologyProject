b0419975d67bbb82031d263d3d27e073c6670574
haris936hk <hariskhan936.hk@gmail.com>
2023-12-08 00:03:02 +0500

    first revision

--- ReactionTime.py
+++ ReactionTime.py
@@ -1,53 +1,83 @@
 import pygame
+import random
 import sys
 
 # Initialize Pygame
 pygame.init()
 
 # Set up display
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pygame Sample")
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Reaction Time Test")
 
 # Set up colors
-black = (0, 0, 0)
-red = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-# Set up the player rectangle
-player_width, player_height = 50, 50
-player_x, player_y = width // 2 - player_width // 2, height // 2 - player_height // 2
-player_speed = 5
+# Set up font
+font = pygame.font.Font(None, 36)
 
-# Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+def display_text(text, color, x, y):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect(center=(x, y))
+    screen.blit(text_surface, text_rect)
 
-    # Handle player input
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_x -= player_speed
-    if keys[pygame.K_RIGHT]:
-        player_x += player_speed
-    if keys[pygame.K_UP]:
-        player_y -= player_speed
-    if keys[pygame.K_DOWN]:
-        player_y += player_speed
-
-    # Boundary checks to keep the player within the window
-    player_x = max(0, min(player_x, width - player_width))
-    player_y = max(0, min(player_y, height - player_height))
-
-    # Clear the screen
-    screen.fill(black)
-
-    # Draw the player rectangle
-    pygame.draw.rect(screen, red, (player_x, player_y, player_width, player_height))
-
-    # Update the display
+def reaction_time_test():
+    screen.fill(WHITE)
     pygame.display.flip()
 
-    # Control the frame rate
-    pygame.time.Clock().tick(60)
+    pygame.time.wait(1000)  # Wait for 1 second before the prompt
+
+    screen.fill(WHITE)
+    display_text("NOW!", BLACK, WIDTH // 2, HEIGHT // 2)
+    pygame.display.flip()
+
+    start_time = pygame.time.get_ticks()
+
+    wait_for_input = True
+    while wait_for_input:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                end_time = pygame.time.get_ticks()
+                reaction_time = (end_time - start_time) / 1000  # Convert to seconds
+                print(f"Your reaction time: {reaction_time:.3f} seconds")
+                wait_for_input = False
+
+def main():
+    print("Welcome to the Reaction Time Test!")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(WHITE)
+
+        play_again_text = font.render("Do you want to play? (Press 'Y' for yes, 'N' for no)", True, BLACK)
+        screen.blit(play_again_text, (WIDTH // 2 - 250, HEIGHT // 2 - 20))
+        pygame.display.flip()
+
+        wait_for_input = True
+        while wait_for_input:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_y:
+                        reaction_time_test()
+                        wait_for_input = False
+                    elif event.key == pygame.K_n:
+                        print("Thanks for playing. Goodbye!")
+                        running = False
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
