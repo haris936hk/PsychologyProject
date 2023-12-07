8c6185b361f923fdd291493face6efb2ba57ee98
haris936hk <hariskhan936.hk@gmail.com>
2023-12-08 00:18:10 +0500

    menu

---
+++ Menu.py
@@ -0,0 +1,58 @@
+import pygame
+import sys
+import subprocess
+
+# Initialize Pygame
+pygame.init()
+
+# Set up display
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Main Menu")
+
+# Set up colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+# Set up font
+font = pygame.font.Font(None, 36)
+
+def display_menu():
+    screen.fill(WHITE)
+
+    menu_text = font.render("Main Menu:", True, BLACK)
+    screen.blit(menu_text, (WIDTH // 2 - 100, 50))
+
+    option1_text = font.render("1. Play Reaction Time Test", True, BLACK)
+    screen.blit(option1_text, (WIDTH // 2 - 200, HEIGHT // 2 - 20))
+
+    option2_text = font.render("2. Quit", True, BLACK)
+    screen.blit(option2_text, (WIDTH // 2 - 200, HEIGHT // 2 + 20))
+
+    pygame.display.flip()
+
+def play_reaction_time_test():
+    try:
+        subprocess.run(["python", "ReactionTime.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+def main_menu():
+    while True:
+        display_menu()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    play_reaction_time_test()
+                elif event.key == pygame.K_2:
+                    print("Goodbye!")
+                    pygame.quit()
+                    sys.exit()
+
+if __name__ == "__main__":
+    main_menu()
